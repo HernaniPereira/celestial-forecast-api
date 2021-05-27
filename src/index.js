2f3119eeb80a44bb0dadd98089b2ec13736d1282
HernaniPereira <pereirahernani9@gmail.com>
2021-05-27 11:18:15 +0100

    login api completed with json web token (jwt)

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 const port = process.env.PORT || 3000;
+const requireAuth = require("./middlewares/requireAuth");
 //const uri = process.env.MONGODB_URI;
 
 const app = express();
@@ -26,8 +27,8 @@ mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`your email: ${req.user.email}`);
 });
 
 app.listen(port, () => {
